59f54bc16c52bdac3bb73437bb255a5d48db8343
honorine22 <higiranez@gmail.com>
2021-12-16 17:37:38 +0200

    Question 6,7 Spying and Mocking

--- src/test/java/rw/ac/rca/termOneExam/utils/CityUtilTest.java
+++ src/test/java/rw/ac/rca/termOneExam/utils/CityUtilTest.java
@@ -2,6 +2,7 @@ package rw.ac.rca.termOneExam.utils;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -9,11 +10,11 @@ import rw.ac.rca.termOneExam.domain.City;
 import rw.ac.rca.termOneExam.repository.ICityRepository;
 import rw.ac.rca.termOneExam.service.CityService;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CityUtilTest {
@@ -47,5 +48,46 @@ public class CityUtilTest {
                 "degree Celsius", cityService.getAll().get(0).getWeather() < 10);
     }
 
+    @Test
+    public void city_contain_given_cities() {
+        ArrayList<City> cities =  spy(ArrayList.class);
+
+        City musanze = new City("Kigali",30);
+        City kigali = new City("Musanze",40);
+
+        cities.add(musanze);
+        cities.add(kigali);
+
+        assertThat(cities).extracting(City::getName).contains("Musanze","Kigali");
+    }
+
+    @Test
+    public void testMocking() {
+        ArrayList<City> arrayListMock =  mock(ArrayList.class);
+
+        when(arrayListMock.size()).thenReturn(5);
+        assertEquals(5, arrayListMock.size());
+
+        when(arrayListMock.get(0)).thenReturn(new City("Kigali",30));
+        assertEquals("Kigali", arrayListMock.get(0).getName());
+
+        verify(arrayListMock, times(1)).get(0);
+
+    }
+
+    @Test
+    public void testSpying() {
+
+        ArrayList<City> arrayListSpy =  spy(ArrayList.class);
+        arrayListSpy.add(new City("Kigali",30));
+
+        arrayListSpy.add(new City("Musanze",40));
+
+
+        when(arrayListSpy.size()).thenReturn(5);
+        assertEquals(5, arrayListSpy.size());
+
+        verify(arrayListSpy).add(new City("Musanze",40));
+    }
 
 }
